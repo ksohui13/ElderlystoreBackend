abcb7f881a31897a360e414120683cc674860e30
Park HeeYeon <hy5877483@naver.com>
2023-08-03 21:53:32 +0900

    배송확인 하는 중

--- customerpage/models.py
+++ customerpage/models.py
@@ -1,16 +1,6 @@
 from typing import Any
 from django.db import models
 from order.models import order, order_detail
-from django.db.models.query import QuerySet
-from django.http import HttpResponseForbidden
-from django.views.generic import ListView
-from django.contrib.auth.decorators import login_required
+
 # Create your models here.
 
-@login_required
-class OrderList(ListView):
-    template_name = "order.html"
-    context_object_name = 'order_list'
-
-    #로그인된 사용자의 정보와 일치하는 주문서 가져와서 queryset에 저장하기
-    
--- customerpage/views.py
+++ customerpage/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
 
+@login_required
+class OrderList(ListView):
+    template_name = "order.html"
+    context_object_name = 'order_list'
+
+    #로그인된 사용자의 정보와 일치하는 주문서 가져와서 queryset에 저장하기
+    
