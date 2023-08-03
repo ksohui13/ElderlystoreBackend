8c9b9b27973c24a670fac7df3c2f3833e32b0847
Park HeeYeon <hy5877483@naver.com>
2023-08-03 21:28:00 +0900

    배송확인 하는 중

--- customerpage/models.py
+++ customerpage/models.py
@@ -4,8 +4,10 @@ from order.models import order, order_detail
 from django.db.models.query import QuerySet
 from django.views import View
 from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required
 # Create your models here.
 
+@login_required
 class OrderList(ListView):
     template_name = "order.html"
     context_object_name = 'order_list'
