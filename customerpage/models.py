dc41edd949285fe8276275d27165194a97744161
Park HeeYeon <hy5877483@naver.com>
2023-08-03 21:43:19 +0900

    배송확인 하는 중

--- customerpage/models.py
+++ customerpage/models.py
@@ -2,7 +2,7 @@ from typing import Any
 from django.db import models
 from order.models import order, order_detail
 from django.db.models.query import QuerySet
-from django.views import View
+from django.http import HttpResponseForbidden
 from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
 # Create your models here.
@@ -12,4 +12,5 @@ class OrderList(ListView):
     template_name = "order.html"
     context_object_name = 'order_list'
 
+    #로그인된 사용자의 정보와 일치하는 주문서 가져와서 queryset에 저장하기
     
