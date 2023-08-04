15a3abf12b3073ce8ca7611ce388e20a24d6cb19
김소희 <111819641+ksohui13@users.noreply.github.com>
2023-08-04 22:52:08 +0900

    확인완료

--- order/urls.py
+++ order/urls.py
@@ -3,3 +3,9 @@ from .views import Order, OrderItem, Shipping
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
+#router url 연결 어디로 할건지 적어줘야해!
+#ex) router.register('list', QuestViewSet) >> 나중에 localhost:8000/api/order/list로 연결됨
+
+# urlpatterns =[
+#     path('', include(router.urls)),
+# ]
